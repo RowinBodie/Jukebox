5fb93d49d7449d314a588951afcc413910cf5d87
Rowin Bodegom <99056991@mydavinci.nl>
2023-01-13 11:21:46 +0100

    added more codecomments

--- htdocs/app/Controllers/Home.php
+++ htdocs/app/Controllers/Home.php
@@ -33,36 +33,45 @@ class Home extends BaseController
             return view('index',["songs"=>$getSongs,"genres"=>$getGenres,"time"=>$time]);
         }        
     }
-
+    // load the genre page
     public function loadGenre($id){
+        // get models
         $getSongsModel = new \App\Models\getSongs;
         $getGenresModel = new \App\Models\getGenres;
         $totalWatchDuration = 0;
+        // gets data needed
         $getSongs = $getSongsModel->where("genre", $id)
                                     ->findAll();
         $getGenres = $getGenresModel->findAll();
+        // calcs the total queue duration
         $minutes = floor($totalWatchDuration / 60);
         $seconds = ($totalWatchDuration % 60);
         $time = sprintf("%02d:%02d", $minutes, $seconds);
         return view('index',["songs"=>$getSongs,"genres"=>$getGenres,"time"=>$time]);
     }
-
+    // saving a playlist redirection
     public function loadSavingPlaylist(){
         return view('save');
     }
-
+    // save the queue to a playlist function
     public function savePlaylist(){
+        // get userid from session
         $userId = session()->get('id');
-
+        // get models
         $playlistModel = new getPlaylists();
         $playlistSongs = new getPlaylistSongs();
+        // get data for database
         $data = [
             'name'     => $this->request->getVar('name'),
             'userId'    => $userId,
         ];
+        // save to database
         $playlistModel->save($data);
+        // get the lastest added database to add songs to
         $createdPlaylist = $playlistModel->orderby('id','DESC')->where("name" , $this->request->getVar('name'))->limit(1)->first();
-        $queue = session()->get("queue");    
+        // get all songs from queue
+        $queue = session()->get("queue");   
+        // save everthing into the newly created playlist 
         foreach($queue as $playlistSong =>$songs){
             $songdata = [
                 'songId' => $songs,
@@ -74,7 +83,7 @@ class Home extends BaseController
         session()->remove("queue");
         return redirect()->to('/home');
     }
-
+    // update playlist name function
     public function updatePlaylist($id){
         $playlistModel = new getPlaylists();
         $name =  $this->request->getVar('name');
@@ -86,13 +95,14 @@ class Home extends BaseController
         $playlistModel->where("id",$id)->replace($data);
         return redirect()->back();
     }
-    
+    // delete song form playlist
     public function deleteSongPlaylist($songId){
         $getplaylistSongs = new getPlaylistSongs();
         $playlistSongs = $getplaylistSongs->where('id', $songId)->first();
         $getplaylistSongs->delete($playlistSongs);
         return redirect()->back();
     }
+    // addsong from the homepage or genre page to a playlist
     public function addSongToPlaylist($songId){
         $getPlaylistSongsModel = new getPlaylistSongs();
         $playlistId =  $this->request->getVar('playlistId');
@@ -103,17 +113,17 @@ class Home extends BaseController
         $getPlaylistSongsModel->save($data);
         return redirect()->back();;
     }
-
+    // add song to queue
     public function addQueueSong($id){
         session()->push("queue", [$id]);
         return redirect()->back();
     }
-
+    // clear the queue
     public function clearQueueSong(){
         session()->remove("queue");
         return redirect()->back();
     }
-    
+    // remove song from queue
     public function removeQueueSong($id){
         $queue = session()->get("queue");
         var_dump($queue);
